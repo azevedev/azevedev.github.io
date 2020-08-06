31bf583b216d00d59f1e52b8f4a1ea4f3076ed69
Matheus Azevedo <azevedooliveiramatheus@gmail.com>
2020-08-06 18:08:47 -0300

    fixing viewport bug

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
 console.log("ready!");
-
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 function changeVisible(){
     console.log("changing...");
     let sectionList = document.querySelectorAll(".hidden");
