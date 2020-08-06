13fdf8f6b008ed20e7094278816db7440e2aa6fc
Matheus Azevedo <azevedooliveiramatheus@gmail.com>
2020-08-06 17:57:43 -0300

    some changes

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,27 @@
 console.log("ready!");
-// document.getElementById("id").addEventListener("click", func);
+
+function changeVisible(){
+    console.log("changing...");
+    let sectionList = document.querySelectorAll(".hidden");
+    sectionList.forEach(function(section){
+        if(isVisible(section)){
+            section.classList.remove("hidden");
+        }else{
+            section.classList.add("hidden");
+        }
+    });
+};
+
+function isVisible(el){
+    let elBox = el.getBoundingClientRect();
+    let dist = -100;
+
+    if(elBox.top - window.innerHeight < dist){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+document.addEventListener("scroll", changeVisible);
+changeVisible();
