58ebf71047d264784e24b09ac3d4c9e78128b7b3
Matheus Azevedo <azevedooliveiramatheus@gmail.com>
2020-08-06 18:15:19 -0300

    asd

--- assets/script.js
+++ assets/script.js
@@ -37,3 +37,9 @@ function ttr(func, time){
         }
     }
 }
+
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
