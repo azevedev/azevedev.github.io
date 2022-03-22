e5c3e1feaf0731e7acc914c0f7e9bc58fa86d44d
Matheus Azevedo <azevedev@gmail.com>
2022-03-22 17:38:01 -0300

    changing some colors and adding a new project

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,25 @@
-console.log("ready!");
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
-function changeVisible(){
-    console.log("changing...");
+
+function changeVisible() {
     let sectionList = document.querySelectorAll(".hidden");
-    sectionList.forEach(function(section){
-        if(isVisible(section)){
+    sectionList.forEach(function(section) {
+        if (isVisible(section)) {
             section.classList.remove("hidden");
-        }else{
+        } else {
             section.classList.add("hidden");
         }
     });
 };
 
-function isVisible(el){
+function isVisible(el) {
     let elBox = el.getBoundingClientRect();
     let dist = -100;
 
-    if(elBox.top - window.innerHeight < dist){
+    if (elBox.top - window.innerHeight < dist) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
@@ -28,10 +27,10 @@ function isVisible(el){
 document.addEventListener("scroll", ttr(changeVisible, 100));
 changeVisible();
 
-function ttr(func, time){
+function ttr(func, time) {
     var total = Date.now();
-    return function(){
-        if((total + time - Date.now()) < 0){
+    return function() {
+        if ((total + time - Date.now()) < 0) {
             func();
             total = Date.now();
         }
