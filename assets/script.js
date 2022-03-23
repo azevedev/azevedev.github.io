747b5710e9ca0e6bcc1c4a26b8dd8854376c561e
Matheus Azevedo <azevedev@gmail.com>
2022-03-23 18:11:45 -0300

    adding a better hero section

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,32 @@
+const loader = document.querySelector('.loader-wrapper');
+const content = document.querySelector('.content');
+const elbody = document.getElementsByTagName('body');
+window.addEventListener('load', function() {
+    this.setTimeout(() => {
+        loader.style.display = "none";
+        content.classList.add('show');
+        document.body.classList.remove('noscroll');
+        document.body.style.maxHeight = "none";
+    }, 3000)
+});
+
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 function changeVisible() {
+
+
+    let element = document.getElementById('welcome');
+    var top_of_screen = window.scrollY;
+
+    if (top_of_screen > window.innerHeight * 1.3) {
+        element.style.display = "none";
+    } else {
+        element.style.display = "block";
+    }
+
+
     let sectionList = document.querySelectorAll(".hidden");
     sectionList.forEach(function(section) {
         if (isVisible(section)) {
